4a42f06b3794004db52dd06ae37c40b8b7a017b1
megan-shah1 <108004216+megan-shah1@users.noreply.github.com>
2022-12-06 16:50:05 -0500

    Add files via upload

---
+++ DataGeneration.cpp
@@ -0,0 +1,163 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <ctime>
+#include <cstdlib>
+#include <set>
+using namespace std;
+
+int main() {
+    ofstream myFile;
+    myFile.open("names.csv"); //names friends
+
+    vector<string> firstNames = {"Tara" ,"Gisselle", "Axel", "Nathanial", "Wendy", "Anya", "Landon", "Macie", "Chaya" ,"Sienna",
+                                 "Luciana", "Amber", "Nina", "Jordyn", "Xiomara", "Elena", "Mina", "Ezra",
+                                 "Saul", "Yasmine", "Bernard", "Jade", "Mckayla", "Madison", "Ahmend", "Janet",
+                                 "Gavven", "Ian", "Ryleigh", "Kaeden", "Caylee", "Arielle", "Lara", "Abigail", "Kamrym",
+                                 "Tiana", "Carolyn", "Adrien", "Mitchell", "Katelyn", "Devyn", "Jaime", "Veronica",
+                                 "Giana", "Richard", "Romeo", "Mallory", "Nolan", "Heidi", "Raven", "Francesca",
+                                 "Frederick", "Troy", "Kara", "Rafael", "Ayaan", "Dominick", "Coby", "Carly", "Christinia",
+                                 "Evelyn", "Juliet", "Carina", "Dwayne", "Kathleen", "Kane", "Mollie", "James", "John",
+                                 "Robert", "Michael", "William", "David", "Richard", "Charles", "Joseph", "Thomas",
+                                 "Christopher", "Daniel", "Paul", "Mark", "Donald", "George", "Kenneth", "Steven",
+                                 "Edward", "Brian", "Ronald", "Anthony", "Kevin", "Jason", "Matthew", "Gary", "Timothy",
+                                 "Jose", "Larry", "Jeffrey", "Frank", "Scott", "Eric", "Stephen", "Andrew", "Raymond", "Gregory",
+                                 "Joshua", "Jerry", "Dennis", "Walter", "Patrick", "Peter", "Harold", "Douglas", "Henry",
+                                 "Carl", "Arthur", "Ryan", "Roger", "Joe", "Juan", "Jack", "Albert", "Jonathan", "Justin",
+                                 "Terry", "Gerald", "Keith", "Samuel", "Willie", "Ralph", "Lawrence", "Nicholas", "Roy",
+                                 "Benjamin", "Bruce", "Brandon", "Adam", "Harry", "Fred", "Wayne", "Billy", "Steve",
+                                 "Louis", "Jeremy", "Aaron", "Randy", "Howard", "Eugene", "Carlos", "Russell", "Bobby",
+                                 "Victor", "Martin", "Ernest", "Phillip", "Todd", "Jesse", "Craig", "Alan", "Shawn",
+                                 "Clarence", "Sean", "Philip", "Chris", "Johny", "Earl", "Jimmy", "Antonio", "Danny",
+                                 "Bryan", "Tony", "Olivia", "Emma", "Charlotte", "Amelia", "Ava", "Sophia", "Isabella",
+                                 "Mia", "Evelyn", "Harper", "Luna", "Camila", "Gianna", "Elizabeth", "Eleanor", "Ella",
+                                 "Abigail", "Sofia", "Avery", "Scarlett", "Emily", "Aria", "Penelope", "Chloe", "Layla",
+                                 "Mila", "Nora", "Hazel", "Madison", "Ellie", "Lily", "Nova", "Isla", "Grace", "Violet",
+                                 "Aurora", "Riley", "Zoey", "Willow", "Emilia", "Stella", "Zoe", "Victoria", "Hannah",
+                                 "Addison", "Leah", "Lucy", "Eliana", "Ivy", "Everly", "Lilian", "Paisley", "Elena",
+                                 "Naomi", "Maya", "Natalie", "Kinsley", "Delilah", "Claire", "Audrey", "Aaliyah", "Ruby",
+                                 "Brooklyn", "Alice", "Aubrey", "Autumn", "Leilani", "Savannah", "Valentina", "Kennedy",
+                                 "Madelyn", "Josephine", "Bella", "Skylar", "Genesis", "Sophie", "Hailey", "Sadie",
+                                 "Natalia", "Quinn", "Caroline", "Allison", "Gabriella", "Anna", "Serenity", "Cora", "Ariana",
+                                 "Emery", "Lydia", "Jade", "Sarah", "Eva", "Adeline", "Madeline", "Piper", "Rylee", "Athena",
+                                 "Peyton", "Vivian", "Clara", "Maria", "Iris", "Julia", "Jasmine"};
+
+    vector<string> lastNames = {"Smith", "Johnson", "Williams", "Brown", "Jones", "Garcia", "Miller", "Davis", "Rodriguez",
+                                "Martinez", "Hernandez", "Lopez", "Gonzalez", "Wilson", "Anderson", "Thomas", "Taylor",
+                                "Moore", "Jackson", "Martin", "Lee", "Perez", "Thompson", "White", "Harris", "Sanchez",
+                                "Clark", "Ramirez", "Lewis", "Robinson", "Walker", "Young", "Allen", "King", "Wright",
+                                "Scott", "Torres", "Nguyen", "Hill", "FLores", "Green", "Adams", "Nelson", "Baker", "Hall",
+                                "Rivera", "Campbell", "Mitchell", "Carter", "Roberts", "Gomez", "Phillips", "Evans", "Turner",
+                                "Diaz", "Parker", "Cruz", "Edwards", "Collins", "Reyes", "Stewart", "Morris", "Morales", "Murphy",
+                                "Cook", "Rogers", "Morgan", "Cooper", "Peterson", "Bailey", "Reed", "Kelly", "Howard",
+                                "Ramos", "Kim", "Cox", "Ward", "Richardson", "Watson", "Brooks", "Chavez", "Wood", "James",
+                                "Bennett", "Gray", "Mendoza", "Ruiz", "Hughes", "Price", "Alvarez", "Castillo", "Sanders",
+                                "Patel", "Myers", "Long", "Ross", "Foster", "Jimenez", "Powell", "Jenkins", "Perry", "Russell",
+                                "Sullivan", "Bell", "Coleman", "Butler", "Henderson", "Barnes", "Gonzales", "Fisher",
+                                "Vasquez", "Simmons", "Romero", "Jordan", "Patterson", "Alexander", "Hamilton", "Graham",
+                                "Reynolds", "Griffin", "Wallace", "Moreno", "West", "Cole", "Hayes", "Bryant", "Herrera",
+                                "Gibson", "Ellis", "Tran", "Medina", "Stevens", "Murray", "Ford", "Castro", "Marshall", "Owens"};
+
+
+    for (int i = 0; i < lastNames.size(); i++)
+    {
+        for (int j = 0; j < firstNames.size(); j++)
+        {
+            myFile << firstNames[j] << " " << lastNames[i] << endl;
+        }
+    }
+
+    for (int i = 0; i < 50; i++)
+    {
+        for (int j = 40; j > 0; j--)
+        {
+            for (int k = 0; k < 40; k++)
+            {
+                if (i != j)
+                {
+                    myFile << firstNames[i] << " " << firstNames[j] << " " << lastNames[k] << endl;
+                }
+            }
+        }
+    }
+    myFile.close();
+
+
+    vector<string> people;
+    ifstream file;
+    file.open("names.csv");
+    string line;
+    while(getline(file, line))
+    {
+        people.push_back(line);
+    }
+    file.close();
+
+    myFile.open("friends.csv");
+    srand(time(0));
+
+    /*
+    for (int i = 1; i <= 100100; i++)
+    {
+        int popularity = (rand() % 2000) + 1; // random number from 1 - 2000
+        cout << popularity << endl;
+        string friends = "";
+
+        for (int j = 0; j <= popularity; j++)
+        {
+            int person = (rand() % 100100);
+            friends += people[person];
+            friends += ",";
+        }
+        friends.pop_back();
+
+        myFile << friends << endl;
+    }
+     */
+
+
+    vector<set<int>> friends;
+
+    // assigns friends to each person and puts it in a vector of sets, each set contains the indexes of the friends
+    for (int i = 0; i < 100100; i++)
+    {
+        int popularity = (rand() % 700) + 1; // random number from 1 - 2000 (number of friends each person has)
+        set<int> temp;
+        for (int j = 0; j <= popularity; j++)
+        {
+            int person = (rand() % 100100);
+            temp.insert(person);
+        }
+        friends.push_back(temp);
+        cout << 100100 - i << endl;
+    }
+
+    for (int i = 0; i < friends.size(); i++)
+    {
+        set<int> :: iterator itr;
+        for (itr = friends[i].begin(); itr != friends[i].end(); ++itr)
+        {
+            friends[*itr].insert(i);
+        }
+        cout << 100100 - i << endl;
+    }
+
+    for (int i = 0; i < friends.size(); i++)
+    {
+        string line = "";
+        line += people[i];
+        line += ",";
+        set<int> :: iterator itr;
+        for (itr = friends[i].begin(); itr != friends[i].end(); ++itr)
+        {
+            line += people[*itr];
+            line += ",";
+        }
+        //line.pop_back();
+        myFile << line << endl;
+    }
+
+    myFile.close();
+    return 0;
+
+}
---
+++ main.cpp
@@ -0,0 +1,259 @@
+//include statements and neccessary packages
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <chrono>
+#include <ctime>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <unordered_set>
+#include <queue>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+using namespace std::chrono;
+
+//depth first search through an inputted adjacency list given two people
+int DFS(map<string, vector<string>> mapPassed, string person1, string person2)
+{
+    //starts the clock at the beginnning of the function
+    auto start = std::chrono::high_resolution_clock::now();
+
+    //a set marks which nodes are visitied while a stack represents the neighbors of each node/person
+    //a counter is initialized to 1 to print each person in alphabetical order based on the traversal
+    int counter = 1;
+    set<string> visited;
+    stack<string> s;
+
+    //first person is inserted into the set and the stack
+    visited.insert(person1);
+    s.push(person1);
+
+    //they are printed and counter is incremented
+    cout << "Depth First Search:" << endl;
+    cout << counter << ") " << person1 << endl;
+    counter++;
+
+    //while the stack still has items in it pop and push
+    while(!s.empty())
+    {
+        //access the friends of the person at the top of the stack and add them into the stack and the visitied set
+        bool doElse = true;
+        string top = s.top();
+        for (int i = 0; i < mapPassed[top].size(); i++)
+        {
+            string myFriend = mapPassed[top][i];
+            if (visited.count(myFriend) == 0)
+            {
+                cout << counter << ") " << myFriend << endl;
+                counter++;
+
+                //if the traversed friend matches the second inputted person, the program breaks out of the loop and
+                //stops printing/traversing
+                if (myFriend == person2)
+                {
+                    return 0;
+                }
+                //otherwise continue inserting people into the set and stack
+                visited.insert(myFriend);
+                s.push(myFriend);
+                doElse = false;
+                break;
+            }
+        }
+
+        //pop people off the stack if the boolean is true and continue
+        if (doElse)
+        {
+            s.pop();
+        }
+    }
+
+    //clock is stopped, duration is calculated and printed
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto durationDepth = duration_cast<std::chrono::microseconds>(stop - start);
+
+    cout << "Time taken for Depth First Search to execute in microseconds: " << durationDepth.count() << endl;
+}
+
+//performs a breadth first search using an inputted adjacency list given two people
+void BFS (map <string, vector<string>> mapPassed, string person1Passed, string person2Passed) {
+    //starts the clock at the beginnning of the function
+    auto start = std::chrono::high_resolution_clock::now();
+
+    //once again, a set marks which people have been visited and a queue is used to track the neighbors
+    //a counter is initialized to 1 to print each person in alphabetical order based on the traversal
+    int counter = 1;
+    std::set<string> visitedPeople;
+    std::queue<string> tempPeopleQ;
+
+    //the first person that is inputted is inserted into the set and pushed into the queue
+    visitedPeople.insert(person1Passed);
+    tempPeopleQ.push(person1Passed);
+
+    cout << "Breadth First Search: " << endl;
+
+    //while the queue is not empty, push and pop neighbors from the queue
+    while(!tempPeopleQ.empty())
+    {
+        //a temporary variable is assigned to the person at the front of the queue
+        string person = tempPeopleQ.front();
+
+        //if the temporary variable node is equal to the second inputted person, print person and break out of program
+        if (person == person2Passed) {
+            cout << counter << ") " << person << endl;
+            counter++;
+            break;
+        }
+
+        //otherwise pop the person off the queue and increment the counter
+        else {
+            cout << counter << ") " << person << endl;
+            counter++;
+            tempPeopleQ.pop();
+        }
+
+        //set the neighbors vector equal to the friends of the person and sort the vector
+        vector<string> neighbors = mapPassed[person];
+        std::sort(neighbors.begin(), neighbors.begin() + neighbors.size());
+
+        //for each item in the neighbor, insert the item with no neighbors into the vector and the set
+        for(string v: neighbors)
+        {
+            if(visitedPeople.count(v) == 0)
+            {
+                visitedPeople.insert(v);
+                tempPeopleQ.push(v);
+            }
+        }
+    }
+
+    //stop clock and calculate the duration of the function
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = duration_cast<std::chrono::microseconds>(stop - start);
+
+    cout << "Time taken for Breadth First Search to execute in microseconds: " << duration.count() << endl;
+}
+
+//heap code, we didn't end up using this!
+void heapifyUp(pair<string, int> a[], int& size, int index)
+{
+    int parentIndex = (index - 1) / 2;
+
+    if (a[index].second >= a[parentIndex].second)
+    {
+        swap(a[index], a[parentIndex]);
+        heapifyUp(a, size, parentIndex);
+    }
+}
+
+void insertHeap(pair<string, int> a[], int& size, pair <string, int> pairPassed)
+{
+    a[size - 1].first = pairPassed.first;
+    a[size - 1].second = pairPassed.second;
+    size++;
+
+    heapifyUp(a, size, size - 1);
+}
+
+
+int main() {
+    //necessary data structures and variables
+    map<string, vector<string>> mapOfFriends;
+    vector<string> associatedFriends;
+    string tempName = "";
+    string exampleFriend = "";
+    string numberOfInputs;
+
+
+    //reads in sample data file with over 100,100 nodes
+    fstream file("project 3 small graph csv.csv");
+    if (file.is_open()) {
+        string line;
+        while (getline(file, line))
+        {
+            // MAKE GRAPH
+            tempName.clear();
+            istringstream stream(line);
+            getline(stream, tempName, ',');
+            mapOfFriends[tempName];
+
+            //creates graph with first word in each line as name and all the following values as friends in the vector
+            while (getline(stream, exampleFriend, ','))
+            {
+                mapOfFriends[tempName].push_back(exampleFriend);
+            }
+        }
+    }
+
+
+    //print menu
+    cout << "***********************************" << endl;
+    cout << "*          GATOR CONNECT          *" << endl;
+    cout << "***********************************" << endl;
+    cout << "GatorConnect is designed to help you find mutual friends to expand your social circle and to find out how other students are connected with each other." << endl;
+    cout << endl;
+    cout << "Input how many operations you wish to perform: " << endl;
+    getline(cin, numberOfInputs);
+    int temp = stoi(numberOfInputs);
+    while (temp != 0) {
+        cout << "Menu:" << endl;
+        cout << "1) Find out how 2 gators are connected using Breadth First Search" << endl;
+        cout << "2) Find out how 2 gators are connected using Depth First Search" << endl;
+        cout << "3) Find how many friends a gator has" << endl;
+        cout << "4) Exit program" << endl;
+        cout << endl;
+        //take in user input for menu options
+        cout << "Enter a menu option to proceed:" << endl;
+        string menuOption;
+        getline(cin, menuOption);
+        cout << endl;
+
+        // if 1 is entered, perform BFS vs DFS and output execution time
+        if (menuOption == "1") {
+            cout << "Please enter Person 1's name: " << endl;
+            string person1;
+            getline(cin, person1);
+            cout << endl;
+            cout << "Please enter Person 2's name: " << endl;
+            string person2;
+            getline(cin, person2);
+            cout << endl;
+
+            BFS(mapOfFriends, person1, person2);
+            cout << endl;
+            temp --;
+        }
+
+        else if (menuOption == "2") {
+            cout << "Please enter Person 1's name: " << endl;
+            string person1;
+            getline(cin, person1);
+            cout << endl;
+            cout << "Please enter Person 2's name: " << endl;
+            string person2;
+            getline(cin, person2);
+            cout << endl;
+
+            DFS(mapOfFriends, person1, person2);
+            cout << endl;
+            temp --;
+        }
+
+            // Enter a gator and find how many friends they have
+        else if (menuOption == "3") {
+            string tempName;
+            cout << "Enter a gator to find how many friends they have: " << endl;
+            getline(cin, tempName);
+            cout << mapOfFriends[tempName].size() << endl;
+            temp --;
+        }
+    }
+
+    return 0;
+}
